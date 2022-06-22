ddcb6abed51ad62c588d542e4385dba90b591da8
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-06-22 09:29:13 -0300

    refactor: move keys to config file

--- services/s3Storage.js
+++ services/s3Storage.js
@@ -1,10 +1,8 @@
 const S3 = require('aws-sdk/clients/s3');
 const { v4 } = require('uuid')
 
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-const region = process.env.AWS_REGION;
-const bucketName = process.env.AWS_BUCKET_NAME
+const config = require('../config/config')
+const { accessKeyId, secretAccessKey, region, bucketName } = config.development
 
 const urlRegex = /^(https?):\/\/[^\s$.?#].[^\s]*$/;
 

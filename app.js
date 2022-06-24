6c25f473e4491e51c187980e1335c3b3583aa699
Juan Pablo Machado <juanpmachadob@gmail.com>
2022-06-24 17:24:24 -0500

    fix: fix json parse in routers

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-indexRouter(app);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+indexRouter(app);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {

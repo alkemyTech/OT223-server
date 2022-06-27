2f8362f5360ce2ad0bb29ac40b3dd1bdf4790d94
Juan Pablo Machado <juanpmachadob@gmail.com>
2022-06-27 17:06:34 -0500

    refactor: change generateJWT promise to async function and add trycatch

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,25 +1,23 @@
 const jwt = require('jsonwebtoken');
 
-const generateJWT = ({
+const generateJWT = async ({
   firstName, lastName, email, image, roleId,
-}) => new Promise((resolve, reject) => {
+}) => {
   const payload = {
     firstName, lastName, email, image, roleId,
   };
-  jwt.sign(
-    payload,
-    process.env.JWT_SECRET_KEY,
-    {
-      expiresIn: process.env.JWT_EXPIRATION_TIME,
-    },
-    (err, token) => {
-      if (err) {
-        reject(new Error('No se pudo generar el token. Por favor, intente nuevamente'));
-      } else {
-        resolve(token);
-      }
-    },
-  );
-});
+  try {
+    const token = await jwt.sign(
+      payload,
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: process.env.JWT_EXPIRATION_TIME,
+      },
+    );
+    return token;
+  } catch (error) {
+    return new Error('No se pudo generar el token. Por favor, intente nuevamente');
+  }
+};
 
 module.exports = generateJWT;

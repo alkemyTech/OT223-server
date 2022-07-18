c7b2ec86d355ff02aed6fc45e03a069e3f3b6dd4
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-07-18 18:32:09 -0300

    feat: create organization model

---
+++ models/organization.js
@@ -0,0 +1,68 @@
+const { Model } = require('sequelize');
+
+const organizationSchema = (DataTypes) => ({
+  id: {
+    primaryKey: true,
+    autoIncrement: true,
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  name: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  image: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  welcomeText: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    field: 'welcome_text',
+  },
+  address: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  phone: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  instagram: {
+    allowNull: true,
+    type: DataTypes.STRING,
+  },
+  facebook: {
+    allowNull: true,
+    type: DataTypes.STRING,
+  },
+  linkedid: {
+    allowNull: true,
+    type: DataTypes.STRING,
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+  },
+  updatedAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+  },
+});
+
+module.exports = (sequelize, DataTypes) => {
+  class Organization extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {}
+  }
+  Organization.init(organizationSchema(DataTypes), {
+    sequelize,
+    modelName: 'Organization',
+    tableName: 'organizations',
+  });
+  return Organization;
+};
